1d13c27838a9843831e7ec737d2e02c7e56339c0
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-12-19 16:50:34 -0300

    Adicionei arquivos referentes a coisa importantes
    Adicionei dois arquivos dos quais contém duas variáveis importantes
    para o funcionamento do jogo.

---
+++ constructs.cpp
@@ -0,0 +1,6 @@
+#include "constructs.h"
+
+namespace GameConstruct {
+bool key = false;
+bool user_exit = false;
+}
---
+++ constructs.h
@@ -0,0 +1,9 @@
+#ifndef CONSTRUCT_H
+#define CONSTRUCT_H
+
+namespace GameConstruct {
+extern bool key;
+extern bool user_exit;
+}
+
+#endif
