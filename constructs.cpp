9ea8e5b9e38f1ee8a03567a8dc91d5dcf4b876f2
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-12-26 15:09:03 -0300

    Apaguei dois arquivos que não servem mais
    Apaguei os arquivos construct.cpp e seu header pois não servem mais o seu proposíto:
    servir de meio de manter registro do que o jogador já fez.

--- constructs.cpp
+++
@@ -1,6 +0,0 @@
-#include "constructs.h"
-
-namespace GameConstruct {
-bool key = false;
-bool user_exit = false;
-}
--- constructs.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSTRUCT_H
-#define CONSTRUCT_H
-
-namespace GameConstruct {
-extern bool key;
-extern bool user_exit;
-}
-
-#endif
